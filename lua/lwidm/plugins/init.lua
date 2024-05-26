33b4d2a96f038b4af98ab01d069df488d7fbd489
Lukas Widmer <lukas@widmer.id.au>
2024-05-26 15:12:21 +0200

    added nvim web-devicons

--- lua/lwidm/plugins/init.lua
+++ lua/lwidm/plugins/init.lua
@@ -1,30 +1,35 @@
 return {
 	-- Detect tabstop and shiftwidth automatically
-	'tpope/vim-sleuth',
+	"tpope/vim-sleuth",
 
 	-- "gc" to comment visual regions/lines
-	{ 'numToStr/Comment.nvim', opts = {} },	
+	{ "numToStr/Comment.nvim", opts = {} },
 
 	-- Adds git related signs to the gutter, as well as utilities for managing changes
-	{ 'lewis6991/gitsigns.nvim',
+	{
+		"lewis6991/gitsigns.nvim",
 		opts = {
 			signs = {
-				add = { text = '+' },
-				change = { text = '~' },
-				delete = { text = '_' },
-				topdelete = { text = '‾' },
-				changedelete = { text = '~' },
+				add = { text = "+" },
+				change = { text = "~" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
 			},
 		},
 	},
 
 	-- Use :Git for git commands in neovim
-	'tpope/vim-fugitive',
+	"tpope/vim-fugitive",
 
 	-- show undo history using :UndotreeToggle
-	{ 'mbbill/undotree',
+	{
+		"mbbill/undotree",
 		config = function()
-			vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
-		end
+			vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+		end,
 	},
+
+	-- nvim-web-devicons
+	{ "nvim-tree/nvim-web-devicons" },
 }
