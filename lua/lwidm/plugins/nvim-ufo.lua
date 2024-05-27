45d3b658d2232cd868472f69af1e51ab5f5ac275
Lukas Widmer <lukas.widmer@maerz.com>
2024-05-27 13:46:54 +0200

    added nvim-ufo.lua

---
+++ lua/lwidm/plugins/nvim-ufo.lua
@@ -0,0 +1,36 @@
+-- lua/lwidm/plugins/nvim-ufo.lua
+
+return {
+	{ "kevinhwang91/promise-async" },
+	{
+		"kevinhwang91/nvim-ufo",
+		dependecies = {
+			"kevinhwang91/promise-async",
+			"neovim/nvim-lspconfig",
+		},
+		lazy = false,
+		config = function()
+			vim.o.foldcolumn = "1" -- '0' is not bad
+			vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+			vim.o.foldlevelstart = 99
+			vim.o.foldenable = true
+
+			local capabilities = vim.lsp.protocol.make_client_capabilities()
+			capabilities.textDocument.foldingRange = {
+				dynamicRegistration = false,
+				lineFoldingOnly = true,
+			}
+			local language_servers = require("lspconfig").util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
+			for _, ls in ipairs(language_servers) do
+				require("lspconfig")[ls].setup({
+					capabilities = capabilities,
+					-- you can add other fields for setting up lsp server in this table
+				})
+			end
+			require("ufo").setup()
+
+			vim.keymap.set("n", "zR", require("ufo").openAllFolds)
+			vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+		end,
+	},
+}
