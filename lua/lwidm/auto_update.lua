8f233eb23bfbf4cde00f86c54f46fcf8d9c6f158
Lukas Widmer <lukas.widmer@maerz.com>
2024-05-28 08:44:01 +0200

    testing autoupdate

--- lua/lwidm/auto_update.lua
+++ lua/lwidm/auto_update.lua
@@ -8,9 +8,8 @@ local function execute_command(command)
 	return result
 end
 
-local function git_check_unstaged_untracked(config_path)
+local function git_check_unstaged_untracked(git_status_cmd)
 	print("Checking status of local git repo ...")
-	local git_status_cmd = "cd " .. config_path .. " && git status --porcelain"
 	local status = execute_command(git_status_cmd)
 	if status ~= "" then
 		print("There are unstaged changes or untracked files. Config didn't autoupdate.")
@@ -19,31 +18,33 @@ local function git_check_unstaged_untracked(config_path)
 	return true
 end
 
-local function git_pull(config_path)
+local function git_pull(git_pull_cmd)
 	print("Pulling from GitHub repo ...")
-	local git_pull_cmd = "cd " .. config_path .. " && git pull"
 	local pull_result = execute_command(git_pull_cmd)
 	print(pull_result)
 end
 
-local function git_push(config_path)
+local function git_push(git_push_cmd)
 	print("Pushing to GitHub repo ...")
-	local git_push_cmd = "cd " .. config_path .. " && git push"
 	local push_result = execute_command(git_push_cmd)
 	print(push_result)
 end
 
 local function update_config()
 	local config_path = vim.fn.stdpath("config")
+	local git_status_cmd = "cd '" .. config_path .. "' && git status --porcelain"
+	local git_pull_cmd = "cd '" .. config_path .. "' && git pull"
+	local git_push_cmd = "cd '" .. config_path .. "' && git push"
+	print(git_status_cmd)
 
 	-- Check for unstaged changes
-	if not git_check_unstaged_untracked(config_path) then
+	if not git_check_unstaged_untracked(git_status_cmd) then
 		return
 	end
 
 	-- Sync with github repo if no unstaged changes or untracked files
-	git_pull(config_path)
-	git_push(config_path)
+	git_pull(git_pull_cmd)
+	git_push(git_push_cmd)
 	print("Config updated successfully.")
 end
 
