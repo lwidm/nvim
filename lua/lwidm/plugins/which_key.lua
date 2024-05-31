92c9b3379742bfd59007af122ae9e1887432caff
Lukas Widmer <lukas.widmer@maerz.com>
2024-05-31 08:13:25 +0200

    removed wrong which key entries

--- lua/lwidm/plugins/which_key.lua
+++ lua/lwidm/plugins/which_key.lua
@@ -10,9 +10,6 @@ local plugin = {
 			require("which-key").register({
 				["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
 				["<leader>d"] = { name = "[D]ebug/[D]ocument", _ = "which_key_ignore" },
-				["<leader>dr"] = { name = "[R]un", _ = "which_key_ignore" },
-				["<leader>dt"] = { name = "[T]est", _ = "which_key_ignore" },
-				["<leader>du"] = { name = "[U]i", _ = "which_key_ignore" },
 				["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
 				["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
 				["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
