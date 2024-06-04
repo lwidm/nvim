4d0baec08134f0365cf545e19067566517636848
Lukas Widmer <lukwidmer@ethz.ch>
2024-06-04 09:24:57 +0200

    primeagen remaps

--- lua/lwidm/init.lua
+++ lua/lwidm/init.lua
@@ -35,6 +35,19 @@ vim.opt.scrolloff = 10
 
 vim.opt.splitbelow = true
 
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.expandtab = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = "~/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.incsearch = true
+vim.opt.hlsearch = true
+
 vim.g.lsp_servers = {
 	clangd = {}, -- c, cpp
 	codelldb = {}, -- c, cpp debugger
