bf9ee806486ce8542d3dc7382cb3f80b5e5713e4
lwidm <lukas@widmer.id.au>
2024-09-27 13:25:23 +0200

    added c/c++ specific tabstop settings

--- lua/lwidm/misc.lua
+++ lua/lwidm/misc.lua
@@ -11,3 +11,10 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 vim.cmd("set background=dark")
 vim.cmd("highlight Normal ctermbg=none guibg=none")
 vim.cmd("highlight TelescopeNormal ctermbg=none guibg=none")
+
+-- Language settings
+-- C/C++ settings (2 spaces for tabs)
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "c", "cpp", "h", "hpp" },
+	command = "setlocal tabstop=2 shiftwidth=2 expandtab",
+})
