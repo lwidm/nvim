90d963f8653b452c9ecdf8d2ba7dc9027b16af04
Lukas Widmer <lukas@widmer.id.au>
2024-10-14 12:28:08 +0200

    new spelling and macro recording

--- lua/lwidm/plugins/lualine.lua
+++ lua/lwidm/plugins/lualine.lua
@@ -3,13 +3,26 @@
 local enabled = true
 local plugin = {
 	{
+		"chrisgrieser/nvim-recorder",
+		dependencies = "rcarriga/nvim-notify", -- optional
+		opts = {}, -- required even with default settings, since it calls `setup()`
+	},
+	{
 		"nvim-lualine/lualine.nvim",
-		dependencies = { { "nvim-tree/nvim-web-devicons", lazy = false } },
+		dependencies = { { "nvim-tree/nvim-web-devicons", "chrisgrieser/nvim-recorder", lazy = false } },
 		config = function()
 			require("lualine").setup({
 				options = {
 					section_separators = "",
 					component_separators = "",
+				},
+				sections = {
+					lualine_y = {
+						{ require("recorder").displaySlots },
+					},
+					lualine_z = {
+						{ require("recorder").recordingStatus },
+					},
 				},
 			})
 		end,
