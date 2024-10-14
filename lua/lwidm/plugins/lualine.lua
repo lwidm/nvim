c98e21deb81ca5db4b9a8a6101482322a91dc73e
lwidm <lukas@widmer.id.au>
2024-10-14 16:57:55 +0200

    warning for which key removed

--- lua/lwidm/plugins/lualine.lua
+++ lua/lwidm/plugins/lualine.lua
@@ -2,28 +2,43 @@
 
 local enabled = true
 local plugin = {
-	{
-		"chrisgrieser/nvim-recorder",
-		dependencies = "rcarriga/nvim-notify", -- optional
-		opts = {}, -- required even with default settings, since it calls `setup()`
-	},
+	-- {
+	-- 	"chrisgrieser/nvim-recorder",
+	-- 	dependencies = "rcarriga/nvim-notify", -- optional
+	-- 	opts = {}, -- required even with default settings, since it calls `setup()`
+	-- },
 	{
 		"nvim-lualine/lualine.nvim",
-		dependencies = { { "nvim-tree/nvim-web-devicons", "chrisgrieser/nvim-recorder", lazy = false } },
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+			-- "chrisgrieser/nvim-recorder",
+		},
+		lazy = false,
 		config = function()
+			local function macro_recording()
+				local recording_register = vim.fn.reg_recording()
+				if recording_register == "" then
+					return ""
+				else
+					return "Recording @ " .. recording_register
+				end
+			end
 			require("lualine").setup({
 				options = {
 					section_separators = "",
 					component_separators = "",
 				},
 				sections = {
-					lualine_y = {
-						{ require("recorder").displaySlots },
-					},
-					lualine_z = {
-						{ require("recorder").recordingStatus },
-					},
+					lualine_c = { "filename", macro_recording },
 				},
+				-- sections = {
+				-- lualine_y = {
+				-- 	{ require("recorder").displaySlots },
+				-- },
+				-- lualine_z = {
+				-- 	{ require("recorder").recordingStatus },
+				-- },
+				-- },
 			})
 		end,
 	},
--- lua/lwidm/plugins/which_key.lua
+++ lua/lwidm/plugins/which_key.lua
@@ -2,39 +2,88 @@
 
 local enabled = true
 local plugin = {
-	{ -- Useful plugin to show you pending keybinds.
+	{
 		"folke/which-key.nvim",
 		event = "VeryLazy",
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+		},
 		config = function()
-			require("which-key").setup()
-			require("which-key").register({
-				["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-				["<leader>d"] = { name = "[D]ebug/[D]ocument", _ = "which_key_ignore" },
-				["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-				["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-				["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-				["<leader>l"] = { name = "[L]anguage", _ = "which_key_ignore" },
-				["<leader>t"] = { name = "[T]rouble", _ = "which_key_ignore" },
-				["<leader>g"] = { name = "[G]it", _ = "which_key_ignore" },
-				["<leader>gd"] = { name = "[G]it [D]iff", _ = "which_key_ignore" },
-				["<leader>gdg"] = { name = "[G]it [D]iff [G]et", _ = "which_key_ignore" },
-				["<leader>gdp"] = { name = "[G]it [D]iff [P]ut", _ = "which_key_ignore" },
-				["<leader>m"] = { name = "[M]arkdown / [M]atlab", _ = "which_key_ignore" },
+			require("which-key").add({
 
-				["<localleader>l"] = { name = "[L]atex", _ = "which_key_ignore" },
-
+				{ "<leader>c", group = "[C]ode" },
+				{ "<leader>d", group = "[D]ebug/[D]ocument" },
+				{ "<leader>r", desc = "[R]ename" },
+				{ "<leader>s", group = "[S]earch" },
+				{ "<leader>l", group = "[L]anguage" },
+				{ "<leader>t", group = "[T]rouble" },
+				{ "<leader>g", group = "[G]it" },
+				{ "<leader>gd", group = "[G]it [D]iff" },
+				{ "<leader>gdg", group = "[G]it [D]iff [G]et" },
+				{ "<leader>gdp", group = "[G]it [D]iff [P]ut" },
+				{ "<leader>m", group = "[M]arkdown / [M]atlab" },
+				{ "<localleader>l", group = "[L]atex" },
 				-- spelling
-				-- ["z="] = { name = "suggest correctly spelled word", _ = "which_key_ignore" },
-				-- ["zg"] = { name = "add word to good word list in spellfile", _ = "which_key_ignore" },
-				-- ["zw"] = { name = "add word to bad word list in spellfile", _ = "which_key_ignore" },
-				-- ["zG"] = { name = "add word to internal good word list", _ = "which_key_ignore" },
-				-- ["zW"] = { name = "add word to internal bad word list", _ = "which_key_ignore" },
-
+				-- { "z=", group = "suggest correctly spelled word" },
+				-- { "zg", desc = "add word to good word list in spellfile" },
+				-- { "zw", desc = "add word to bad word list in spellfile" },
+				-- { "zG", desc = "add word to internal good word list" },
+				-- { "zW", desc = "add word to internal bad word list" },
 				-- obsidian
-				["<leader>o"] = { name = "obsidian / open folder", _ = "which_key_ignore" },
+				{ "<leader>o", group = "obsidian / open folder" },
+
+				{ "@", group = "Play Macro" },
+				{ "@a", desc = "Play Macro: a" }, -- Example
+				-- Macro record
+				{ "q", group = "Record Macro" },
+				{ "qa", desc = "Record Macro: a" }, -- Example
 			})
 		end,
 	},
+	-- { -- Useful plugin to show you pending keybinds.
+	-- 	"folke/which-key.nvim",
+	-- 	event = "VeryLazy",
+	-- 	config = function()
+	-- 		require("which-key").setup()
+	-- 		require("which-key").register({
+	-- 			["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
+	-- 			["<leader>d"] = { name = "[D]ebug/[D]ocument", _ = "which_key_ignore" },
+	-- 			["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
+	-- 			["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
+	-- 			["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
+	-- 			["<leader>l"] = { name = "[L]anguage", _ = "which_key_ignore" },
+	-- 			["<leader>t"] = { name = "[T]rouble", _ = "which_key_ignore" },
+	-- 			["<leader>g"] = { name = "[G]it", _ = "which_key_ignore" },
+	-- 			["<leader>gd"] = { name = "[G]it [D]iff", _ = "which_key_ignore" },
+	-- 			["<leader>gdg"] = { name = "[G]it [D]iff [G]et", _ = "which_key_ignore" },
+	-- 			["<leader>gdp"] = { name = "[G]it [D]iff [P]ut", _ = "which_key_ignore" },
+	-- 			["<leader>m"] = { name = "[M]arkdown / [M]atlab", _ = "which_key_ignore" },
+	--
+	-- 			["<localleader>l"] = { name = "[L]atex", _ = "which_key_ignore" },
+	--
+	-- 			-- spelling
+	-- 			-- ["z="] = { name = "suggest correctly spelled word", _ = "which_key_ignore" },
+	-- 			-- ["zg"] = { name = "add word to good word list in spellfile", _ = "which_key_ignore" },
+	-- 			-- ["zw"] = { name = "add word to bad word list in spellfile", _ = "which_key_ignore" },
+	-- 			-- ["zG"] = { name = "add word to internal good word list", _ = "which_key_ignore" },
+	-- 			-- ["zW"] = { name = "add word to internal bad word list", _ = "which_key_ignore" },
+	--
+	-- 			-- obsidian
+	-- 			["<leader>o"] = { name = "obsidian / open folder", _ = "which_key_ignore" },
+	-- 		})
+	-- 	end,
+	-- },
 }
 if enabled then
 	return plugin
