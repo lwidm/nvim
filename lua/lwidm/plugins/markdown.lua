66d159859b81f6c1f6b7dd234947ec4fd78239b1
lwidm <lukas@widmer.id.au>
2025-01-06 10:14:03 +0100

    no clue

--- lua/lwidm/plugins/markdown.lua
+++ lua/lwidm/plugins/markdown.lua
@@ -2,20 +2,35 @@
 
 local enabled = true
 local plugin = {
-	-- install without yarn or npm
+
 	{
-		"iamcco/markdown-preview.nvim",
-		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-		ft = { "markdown" },
-		build = function()
-			vim.fn["mkdp#util#install"]()
-		end,
+		"OXY2DEV/markview.nvim",
+		lazy = false, -- Recommended
+		-- ft = "markdown" -- If you decide to lazy-load anyway
+
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-tree/nvim-web-devicons",
+		},
 		keys = {
-			-- stylua: ignore start
-			{ "<leader>mp", ":MarkdownPreviewToggle<CR>", mode = "n", silent = true, desc = "[M]arkdown [P]review toggle"},
-			-- stylua: ignore end
+			{ "<leader>mp", "<cmd>Markview<cr>", desc = "[M]arkdown [P]review toggle" },
 		},
 	},
+
+	-- install without yarn or npm
+	-- {
+	-- 	"iamcco/markdown-preview.nvim",
+	-- 	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+	-- 	ft = { "markdown" },
+	-- 	build = function()
+	-- 		vim.fn["mkdp#util#install"]()
+	-- 	end,
+	-- 	keys = {
+	-- 		-- stylua: ignore start
+	-- 		{ "<leader>mp", ":MarkdownPreviewToggle<CR>", mode = "n", silent = true, desc = "[M]arkdown [P]review toggle"},
+	-- 		-- stylua: ignore end
+	-- 	},
+	-- },
 
 	-- install with yarn or npm
 	-- {
