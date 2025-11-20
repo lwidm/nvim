5768485e5aaaf8842d587cb3f0c72290a670cbf6
lwidm <lukas@widmer.id.au>
2025-11-20 11:19:59 -0800

    imporved debuggin capability

--- lua/lwidm/plugins/debug.lua
+++ lua/lwidm/plugins/debug.lua
@@ -5,31 +5,92 @@ local plugin = {
 	-- nvim-dap
 	{
 		"mfussenegger/nvim-dap",
+		dependencies = {
+			"rcarriga/nvim-dap-ui",
+			"nvim-neotest/nvim-nio",
+			"jay-babu/mason-nvim-dap.nvim",
+			"theHamsta/nvim-dap-virtual-text",
+		},
 		keys = {
 			-- stylua: ignore start
 			{ "<F5>", function() require("dap").continue() end, mode = "n", desc = "debug continue", },
-			{ "<F8>", function() require('dap').step_over() end, mode = "n", desc = "debug step over", },
-			{ "<F7>", function() require('dap').step_into() end, mode = "n", desc = "debug step into", },
-			{ "<S-F8>", function() require('dap').step_out() end, mode = "n", desc = "debug step out", },
-			{ "<leader>db", function() require("dap").toggle_breakpoint() end, mode = "n", desc = "[D]ebug ui [T]oggle", },
+			{ "<F10>", function() require('dap').step_over() end, mode = "n", desc = "debug step over", },
+			{ "<F11>", function() require('dap').step_into() end, mode = "n", desc = "debug step into", },
+
+			{ "<S-F11>", function() require('dap').step_out() end, mode = "n", desc = "debug step out", },
+			{ "<F23>", function() require('dap').step_out() end, mode = "n", desc = "debug step out", },
+
+			{ "<S-F5>", function() require('dap').terminate() end, mode = "n", desc = "debug step out", },
+			{ "<F17>", function() require('dap').terminate() end, mode = "n", desc = "debug step out", },
+
+			{ "<C-S-F5>", function() require('dap').restart() end, mode = "n", desc = "debug step out", },
+			{ "<S-F10>", function() require('dap').restart() end, mode = "n", desc = "debug step out", },
+			{ "<F22>", function() require('dap').restart() end, mode = "n", desc = "debug step out", },
+
+			{ "<F9>", function() require("dap").toggle_breakpoint() end, mode = "n", desc = "[D]ebug [B]reakpoint toggle", },
+			{ "<leader>db", function() require("dap").toggle_breakpoint() end, mode = "n", desc = "[D]ebug [B]reakpoint toggle", },
+			{ "<leader>dc", function() require("dap").clear_breakpoints() end, mode = "n", desc = "[D]ebug [C]lear breakpoints", },
 			{ "<Leader>dr", function() require('dap').repl.open() end, mode = "n", desc = "[D]ebug open [R]epl", },
 			{ "<Leader>dl", function() require('dap').run_last() end, mode = "n", desc = "[D]ebug run [L]ast", },
-			{ "<Leader>dh", function()
-			  require('dap.ui.widgets').hover()
-			end, mode = {'n', 'v'}, desc = "[D]ap ui [H]over", },
-			{ "<Leader>dp", function()
-			  require('dap.ui.widgets').preview()
-			end, mode = {'n', 'v'}, desc = "[D]ap ui [P]review", },
+			{ "<Leader>dh", function() require('dap.ui.widgets').hover() end, mode = {'n', 'v'}, desc = "[D]ap ui [H]over", },
+			{ "<Leader>dp", function() require('dap.ui.widgets').preview() end, mode = {'n', 'v'}, desc = "[D]ap ui [P]review", },
 			{ "<Leader>df", function()
-			  local widgets = require('dap.ui.widgets')
-			  widgets.centered_float(widgets.frames)
-			end, mode = "n", desc = "[D]ap ui centered float [F]rames ?", },
+			                    local widgets = require('dap.ui.widgets') widgets.centered_float(widgets.frames)
+				            end,
+				mode = "n", desc = "[D]ap ui centered float [F]rames ?", },
 			{ "<Leader>ds", function()
-			  local widgets = require('dap.ui.widgets')
-			  widgets.centered_float(widgets.scopes)
-			end, mode = "n", desc = "[D]ap ui centered float [S]cope?", },
+			                    local widgets = require('dap.ui.widgets')
+			                    widgets.centered_float(widgets.scopes)
+			                end,
+				mode = "n", desc = "[D]ap ui centered float [S]cope?", },
+			{ "]b", function() require("dap").list_breakpoints() end, mode = "n", desc = "goto next [B]reakpoint",},
+			{ "[b", function() require("dap").list_breakpoints() end, mode = "n", desc = "goto previous [B]reakpoint",},
 			-- stylua: ignore start
 		},
+		config = function()
+			local dap = require("dap")
+
+			dap.adapters.codelldb = {
+				type = "server",
+				port = "${port}",
+				executable = {
+					command = "codelldb",
+					args = { "--port", "${port}" },
+				},
+				name = "lldb",
+			}
+
+			dap.configurations = {
+				c = {
+					{
+						name = "Launch file",
+						type = "cppdbg",
+						request = "launch",
+						program = function()
+							return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+						end,
+						cwd = "${workspaceFolder}",
+						stopAtEntry = false,
+						MIMode = "lldb",
+					},
+					{
+						name = "Attach to lldbserver :1234",
+						type = "cppdbg",
+						request = "launch",
+						MIMode = "lldb",
+						miDebuggerServerAddress = "localhost:1234",
+						miDebuggerPath = "/usr/bin/lldb",
+						cwd = "${workspaceFolder}",
+						program = function()
+							return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+						end,
+					},
+				},
+			}
+
+			-- Use same configuration for C
+			dap.configurations.cpp = dap.configurations.c
+		end,
 	},
 
 	-- nvim-nio
@@ -105,9 +166,17 @@ local plugin = {
 			"mfussenegger/nvim-dap",
 			"rcarriga/nvim-dap-ui",
 		},
-		opts = {
-			handlers = {},
-		},
+		config = function()
+			require("mason-nvim-dap").setup({
+				ensure_installed = { "cppdbg" },
+				automatic_installation = true,
+				handlers = {
+					function(config)
+						require("mason-nvim-dap").default_setup(config)
+					end,
+				},
+			})
+		end,
 	},
 }
 if enabled then
